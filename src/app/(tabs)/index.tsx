00f878859c65d71a21bc88c014060f29c559b0dc
d3r3alk3ll0 <derealkello011@gmail.com>
2026-09-10 04:02:31 +0000

    Add RecentMeals component to HomeScreen and adjust global styles for platform-specific padding

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,17 +1,25 @@
 import HomeHeader from "@/components/HomeHeader";
 import MacroGrid from "@/components/MacroGrid";
+import RecentMeals from "@/components/RecentMeals";
 import { globalStyles } from "@/styles/global";
+import { StatusBar } from 'expo-status-bar';
 import { ScrollView, Text } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function HomeScreen() { 
   return (
-    <SafeAreaProvider>
-        <ScrollView style={globalStyles.container}>
-          <Text style={globalStyles.title}>NutriTrack</Text>
+    <SafeAreaProvider >
+        <ScrollView 
+            style={globalStyles.container} 
+            contentContainerStyle={{ paddingBottom: 120 }}
+        >
+        <Text style={globalStyles.title}>NutriTrack</Text>
+        <StatusBar style="light"/>
         <HomeHeader />
         <MacroGrid />
-        </ScrollView>
+        <RecentMeals />
+      </ScrollView>
+      
     </SafeAreaProvider>
   )
 };
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import { Platform, StyleSheet } from "react-native";
 
 export const colors = {
     background: '#1a1a2e',
@@ -14,7 +14,7 @@ export const globalStyles = StyleSheet.create( {
     container: {
         flex: 1,
         backgroundColor: colors.background,
-        paddingTop: 60,
+        paddingTop: Platform.OS === 'ios' ? 60 : 20,
         paddingHorizontal: 20,
         paddingBottom: 100,
     },
